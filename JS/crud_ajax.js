b5e4d004fc2956c79232010400e4239f0d12cd2d
Emiliano Ceroncd Waifu-Administratorgit config --global user.name Emiliano <hceronj1400@alumno.ipn.mx>
2021-07-22 08:42:17 -0500

    Add json, html and JS files for crud ajax

---
+++ JS/crud_ajax.js
@@ -0,0 +1,5 @@
+const d = document,
+  $table = d.querySelector(".crud-table"),
+  $form = d.querySelector(".crud-form"),
+  $title = d.querySelector(".crud-title"),
+  $template = d.getElementById("crud-template");
